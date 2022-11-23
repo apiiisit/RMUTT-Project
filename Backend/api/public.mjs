2463ce62171b05aeb1ff07869617040ba43fe712
Passawat Noraman <notrealpaz@gmail.com>
2022-11-24 01:15:16 +0700

    login part not finish yet

--- Backend/api/public.mjs
+++ Backend/api/public.mjs
@@ -1,11 +1,32 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
 const publicapi = Router();
 
-publicapi.get('/', (req, res) => res.status(200).send('200'));
+const GenerateToken = (data) => jwt.sign(data, process.env.TOKEN_SECRET, { algorithm: 'HS256' });
 
-// return all students
-publicapi.get('/students', (req, res) => {
-  res.send('Students List');
-});
+// Status/Connection Check
+publicapi.get('/', async (req, res) => res.json({ error: false }));
+
+// login
+// publicapi.post('/login', async (req, res) => {
+//   try {
+//     // We don't have access to RMUTT Database
+//     const url = 'https://covid19.rmutt.ac.th/model/login.model.php';
+//     const formData = (new URLSearchParams({ username: req.body.username, password: req.body.password })).toString();
+//     const postreq = await fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded',
+//         'user-agent': 'rmutt-project contact at notrealpaz@gmail.com'
+//       },
+//       body: formData
+//     });
+//     const data = await postreq.json();
+//     if (data.error) return res.status(500).json({ error: 'username or password is incorrect' });
+//     return res.json({ error: false, token: GenerateToken(req.body.username) });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// });
 
 export default publicapi;
--- Backend/index.mjs
+++ Backend/index.mjs
@@ -2,11 +2,14 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import publicapi from './api/public.mjs';
 import adminapi from './api/admin.mjs';
+import { randomBytes } from 'crypto';
 dotenv.config();
 const app = express();
 const port = process.env.port;
 process.env.MASTERPASSWORDB64 = Buffer.from(process.env.MASTERPASSWORD).toString('base64');
-
+if (process.env.NODE_ENV === 'development') {
+  process.env.TOKEN_SECRET = randomBytes(64).toString('hex');
+}
 app.use(express.json());
 app.use('/', publicapi);
 app.use('/admin', adminapi);
